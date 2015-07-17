2700a38ca016634909fc8c8445c31f42d53b8921
joe hobson <joe@navigationnorth.com>
2015-07-17 16:30:32 -0500

    only show social buttons when there's data

--- header.php
+++ header.php
@@ -76,21 +76,32 @@
 				?>
 
                 <div class="col-md-11 col-sm-12 col-xs-5 col-xs-offset-2 col-md-offset-1 soclize">
-                	<a href="<?php echo twitter_url;?>" target="_blank">
-                    	<span class="socl_icns fa-stack"><i class="fa fa-twitter fa-stack-2x"></i></span>
-                    </a>
-                    <a href="<?php echo facebook_url;?>" target="_blank">
-                    	<span class="socl_icns fa-stack"><i class="fa fa-facebook fa-stack-2x"></i></span>
-                    </a>
-                    <a href="<?php echo yotube_url;?>" target="_blank">
-                    	<span class="socl_icns fa-stack"><i class="fa fa-youtube-play fa-stack-2x"></i></span>
-                    </a>
-                    <a href="<?php echo google_url;?>" target="_blank">
-                    	<span class="socl_icns fa-stack"><i class="fa fa-google-plus fa-stack-2x"></i></span>
-                    </a>
-                    <a href="<?php echo linktonwltr;?>" target="_blank">
-                    	<span class="socl_icns fa-stack"><i class="fa fa-envelope fa-stack-2x"></i></span>
-                    </a>
+
+        			<?php if ( twitter_url !== "" ) : ?>
+                        <a href="<?php echo twitter_url;?>" target="_blank" title="Follow Us on Twitter">
+        				    <span class="socl_icns fa-stack"><i class="fa fa-twitter fa-stack-2x"></i></span>
+                        </a>
+        			<?php endif; ?>
+        			<?php if ( facebook_url !== "" ) : ?>
+            			<a href="<?php echo facebook_url;?>" target="_blank" title="Follow Us on Facebook">
+            			    <span class="socl_icns fa-stack"><i class="fa fa-facebook fa-stack-2x"></i></span>
+            			</a>
+        			<?php endif; ?>
+        			<?php if ( yotube_url !== "" ) : ?>
+            			<a href="<?php echo yotube_url;?>" target="_blank" title="Subscribe on YouTube">
+            			    <span class="socl_icns fa-stack"><i class="fa fa-youtube-play fa-stack-2x"></i></span>
+            			</a>
+        			<?php endif; ?>
+        			<?php if ( google_url !== "" ) : ?>
+            			<a href="<?php echo google_url;?>" target="_blank" title="Follow Us on Google+">
+            			    <span class="socl_icns fa-stack"><i class="fa fa-google-plus fa-stack-2x"></i></span>
+            			</a>
+        			<?php endif; ?>
+        			<?php if ( linktonwltr !== "" ) : ?>
+            			<a href="<?php echo linktonwltr;?>" target="_blank" title="Sign Up for our Newsletter">
+            			    <span class="socl_icns fa-stack"><i class="fa fa-envelope fa-stack-2x"></i></span>
+            			</a>
+        			<?php endif; ?>
                 </div>
 
             </div>
