623edb5ecde73fb81807164900f37f4d1219b08c
John Paul Balagolan <johnpaulbalagolan@gmail.com>
2016-05-12 22:38:15 +0800

    create publication subsection template

---
+++ page-templates/publication-subsection-template.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Template Name: Publication Subsection Template
+ */
+?>
+<?php get_header();?>
+ <div id="content" class="row site-content" tabindex="-1">
+
+	<?php
+		global $post;
+		$page_id = get_the_ID();
+	?>
+
+	<div class="col-md-12 c ol-sm-12 col-xs-12 padding_left padding_right">
+    	<h2 class="pblctn_hed"><?php echo $post->post_title;?></h2>
+
+        <?php
+			while ( have_posts() ) : the_post();
+				get_template_part( 'content', 'page' );
+			endwhile;
+		?>
+	</div>
+
+</div>
+<?php get_footer();?>
