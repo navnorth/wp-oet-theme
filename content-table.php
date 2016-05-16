774944f19243a869427b866327de55f36f5c3c88
John Paul Balagolan <johnpaulbalagolan@gmail.com>
2016-05-16 22:28:33 +0800

    display sub pages not using publication subsection template as links

--- content-table.php
+++ content-table.php
@@ -8,6 +8,8 @@ else {
     $parent_id = $post->post_parent;
     if ($parent_id>0)
             $withChild = true;
+
+$sublinks = array();
 }
 ?>
 <div class="right_sid_mtr program_toc_box" id="toc">
@@ -18,28 +20,44 @@ else {
                 if ($subpages) {
                     $index = 0;
                     foreach($subpages as $spage) {
-                            $index++;
-                            if (($index % 2)==1) {
+                            $template = get_post_meta($spage->ID, '_wp_page_template', true);
+                            if ($template == "page-templates/publication-subsection-template.php") {
+                                $index++;
+                                if (($index % 2)==1) {
+                                    ?>
+                                    <div class="row">
+                                    <?php
+                                }
+                                $featured_image = wp_get_attachment_url( get_post_thumbnail_id($spage->ID) );
                                 ?>
-                                <div class="row">
-                                <?php
-                            }
-                            $featured_image = wp_get_attachment_url( get_post_thumbnail_id($spage->ID) );
-                            ?>
-                            <div class="col-md-6" style="margin-bottom: 30px;">
-                                <a href="<?php echo get_page_link($spage->ID); ?>" onmousedown="_sendEvent('Outbound','tech.ed.gov','<?php echo get_page_link($spage->ID); ?>',0);">
-                                    <button class="btn btn-large toc-button">
-                                        <?php if ($featured_image): ?>
-                                            <img src="<?php echo $featured_image; ?>" height="60px" style="margin-right: 15px;">
-                                        <?php endif; ?>
-                                         <?php echo $spage->post_title; ?>
-                                    </button>
-                                </a>
-                            </div>
-                            <?php if (($index % 2)==0) { ?>
+                                <div class="col-md-6" style="margin-bottom: 30px;">
+                                    <a href="<?php echo get_page_link($spage->ID); ?>" onmousedown="_sendEvent('Outbound','tech.ed.gov','<?php echo get_page_link($spage->ID); ?>',0);">
+                                        <button class="btn btn-large toc-button">
+                                            <?php if ($featured_image): ?>
+                                                <img src="<?php echo $featured_image; ?>" height="60px" style="margin-right: 15px;">
+                                            <?php endif; ?>
+                                             <?php echo $spage->post_title; ?>
+                                        </button>
+                                    </a>
                                 </div>
-                            <?php } ?>
+                                <?php if (($index % 2)==0) { ?>
+                                    </div>
+                                <?php } ?>
+                            <?php } else {
+                                $sublinks[] = $spage;
+                            } ?>
                             <?php
+                    }
+                    if (!empty($sublinks)) {
+                        echo "<div class='clearfix'></div>";
+                        echo "<hr>";
+                        $subindex = 0;
+                        foreach($sublinks as $sublink) {
+                            $subindex++;
+                            echo '<a href="'.get_page_link($sublink->ID).'" onmousedown="_sendEvent(\'Outbound\',\'tech.ed.gov\',\''.get_page_link($sublink->ID).'\',0);">'.$sublink->post_title.'</a>';
+                            if ($subindex<count($sublinks))
+                                echo ' / ';
+                        }
                     }
                 } else {
                     ?>
@@ -58,29 +76,45 @@ else {
                             
                             $index=0;
                             foreach($subpages as $spage) {
-                                $index++;
-                                if (($index % 3)==1) { ?>
-                                    <div class="row">
-                                <?php }
-                                $featured_image = wp_get_attachment_url( get_post_thumbnail_id($spage->ID) );
-                                ?>
-                                
-                                <div class="col-md-4" style="margin-bottom: 15px; margin-top: 15px;">
-                                    <a href="<?php echo get_page_link($spage->ID); ?>" onmousedown="_sendEvent('Outbound','tech.ed.gov','<?php echo get_page_link($spage->ID); ?>',0);">
-                                        <button class="btn btn-large toc-small-button">
-                                            <?php if ($featured_image){ ?>
-                                            <img src="<?php echo $featured_image; ?>" height="30px" style="margin-right: 15px;">
-                                            <?php } ?>
-                                             <?php echo $spage->post_title; ?>
-                                        </button>
-                                    </a>
-                                </div>
-                                
-                                <?php if (($index % 3)==0) { ?>
+                                $template = get_post_meta($spage->ID, '_wp_page_template', true);
+                                if ($template == "page-templates/publication-subsection-template.php") {
+                                    $index++;
+                                    if (($index % 3)==1) { ?>
+                                        <div class="row">
+                                    <?php }
+                                    $featured_image = wp_get_attachment_url( get_post_thumbnail_id($spage->ID) );
+                                    ?>
+                                    
+                                    <div class="col-md-4" style="margin-bottom: 15px; margin-top: 15px;">
+                                        <a href="<?php echo get_page_link($spage->ID); ?>" onmousedown="_sendEvent('Outbound','tech.ed.gov','<?php echo get_page_link($spage->ID); ?>',0);">
+                                            <button class="btn btn-large toc-small-button">
+                                                <?php if ($featured_image){ ?>
+                                                <img src="<?php echo $featured_image; ?>" height="30px" style="margin-right: 15px;">
+                                                <?php } ?>
+                                                 <?php echo $spage->post_title; ?>
+                                            </button>
+                                        </a>
                                     </div>
-                                <?php } ?>
+                                    
+                                    <?php if (($index % 3)==0) { ?>
+                                        </div>
+                                    <?php } ?>
+                                <?php } else {
+                                    $sublinks[] = $spage;
+                                } ?>
                             <?php
                             }
+                        if (!empty($sublinks)) {
+                            echo "<div class='clearfix'></div>";
+                            echo "<hr>";
+                            $subindex = 0;
+                            foreach($sublinks as $sublink) {
+                                $subindex++;
+                                echo '<a href="'.get_page_link($sublink->ID).'" onmousedown="_sendEvent(\'Outbound\',\'tech.ed.gov\',\''.get_page_link($sublink->ID).'\',0);">'.$sublink->post_title.'</a>';
+                                if ($subindex<count($sublinks))
+                                    echo ' / ';
+                            }
+                        }
                     }
                     ?>
                     </div>
