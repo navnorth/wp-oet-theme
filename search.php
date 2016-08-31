2767a3c98e76b7232d265be11c3801be255349a0
joe hobson <joe@navigationnorth.com>
2016-08-31 18:19:30 -0500

    integrate Story custom search display

--- search.php
+++ search.php
@@ -18,47 +18,51 @@ get_header(); ?>
 			</header>
 
 			<?php while ( have_posts() ) : the_post(); ?>
-				
-				<?php 
-					//get_template_part( 'content', get_post_format() );
-				?>
-				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-					<?php if ( is_sticky() && is_home() && ! is_paged() ) : ?>
-                    <div class="featured-post">
-                        <?php _e( 'Featured post', 'twentytwelve' ); ?>
-                    </div>
-                    <?php endif; ?>
-                    <header class="entry-header">
-                        
-                        <?php if ( is_single() ) : ?>
-                        <h2 class="entry-title"><?php the_title(); ?></h1>
+
+                <?php if (get_post_type() == 'stories' && function_exists('get_story_template_part')) { ?>
+                    <?php get_story_template_part( 'content', 'search' ); ?>
+
+                <?php } else { ?>
+
+    				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+                        <?php if ( is_sticky() && is_home() && ! is_paged() ) : ?>
+                            <div class="featured-post">
+                                <?php _e( 'Featured post', 'twentytwelve' ); ?>
+                            </div>
+                        <?php endif; ?>
+
+                        <header class="entry-header">
+                            <?php if ( is_single() ) : ?>
+                                <h3 class="entry-title"><?php the_title(); ?></h3>
+                            <?php else : ?>
+                                <h3 class="entry-title">
+                                    <a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a>
+                                </h3>
+                            <?php endif; // is_single() ?>
+                        </header><!-- .entry-header -->
+
+                        <?php if ( is_search() ) : // Only display Excerpts for Search ?>
+                            <div class="entry-summary">
+                                <?php the_excerpt(); ?>
+                            </div><!-- .entry-summary -->
                         <?php else : ?>
-                        <h2 class="entry-title">
-                            <a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a>
-                        </h2>
-                        <?php endif; // is_single() ?>
-                        
-                    </header><!-- .entry-header -->
-            
-                    <?php if ( is_search() ) : // Only display Excerpts for Search ?>
-                    <div class="entry-summary">
-                        <?php the_excerpt(); ?>
-                    </div><!-- .entry-summary -->
-                    <?php else : ?>
-                    <div class="entry-content">
-                        <?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'twentytwelve' ) ); ?>
-                        <?php wp_link_pages( array( 'before' => '<div class="page-links">' . __( 'Pages:', 'twentytwelve' ), 'after' => '</div>' ) ); ?>
-                    </div><!-- .entry-content -->
-                    <?php endif; ?>
-            
-                </article><!-- #post -->
+                            <div class="entry-content">
+                                <?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'twentytwelve' ) ); ?>
+                                <?php wp_link_pages( array( 'before' => '<div class="page-links">' . __( 'Pages:', 'twentytwelve' ), 'after' => '</div>' ) ); ?>
+                            </div><!-- .entry-content -->
+                        <?php endif; ?>
+
+                    </article><!-- #post -->
+
+                <?php } ?>
 
 			<?php endwhile; ?>
 
 
 		<?php else : ?>
 
-			<article id="post-0" class="post no-results not-found">
+			<article id="post-0" class="post no-results not-found col-md-12">
 				<header class="entry-header">
 					<h1 class="entry-title"><?php _e( 'Nothing Found', 'twentytwelve' ); ?></h1>
 				</header>
