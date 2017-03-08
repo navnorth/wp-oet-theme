dae802bea696e50e4def656f5b8dc6b4b7c8e4fe
John Paul Balagolan <johnpaulbalagolan@gmail.com>
2017-03-08 23:45:15 +0800

    update oet_accordion shortcode to support multiple instances on a page

--- theme-functions/theme-shortcode.php
+++ theme-functions/theme-shortcode.php
@@ -32,9 +32,16 @@ function disruptive_content_fun($attr, $content = null)
 add_shortcode('oet_accordion_group', 'oet_accordion_group_func');
 function oet_accordion_group_func($atts, $content = null)
 {
-	//extract($atts);
+	$accordion_id = "accordion";
+	
+	if (!empty($atts)) {
+		extract($atts);
+		if ($id)
+			$accordion_id = $id;
+	}
+	
 	$return = '';
-	$return .= '<div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">';
+	$return .= '<div class="panel-group" id="'.$accordion_id.'" role="tablist" aria-multiselectable="true">';
 			$content = str_replace( "<p>","", $content );
 			$content = str_replace( "</p>","", $content );
 			$return .= do_shortcode($content);
@@ -45,6 +52,8 @@ function oet_accordion_group_func($atts, $content = null)
 add_shortcode('oet_accordion', 'oet_accordion_func');
 function oet_accordion_func($atts, $content = null)
 {
+$group_id = "accordion";
+
   extract($atts);
   $return = '';
 
@@ -66,13 +75,13 @@ function oet_accordion_func($atts, $content = null)
 				  $uptcls = '';
 			  }
 
-			  $return .= '<a class="'.$class.'" data-toggle="collapse" data-parent="#accordion" href="#collapse'. $accordion_series .'" aria-expanded="false" aria-controls="collapse'. $accordion_series .'">';
+			  $return .= '<a class="'.$class.'" data-toggle="collapse" data-parent="#'.$group_id.'" href="#collapse'. $group_id. $accordion_series .'" aria-expanded="false" aria-controls="collapse'. $accordion_series .'">';
 			  $return .= $title;
 			$return .= '</a>';
 		 $return .= ' </h5>';
 		$return .= '</div>';
 
-		$return .= '<div id="collapse'. $accordion_series .'" class="panel-collapse collapse '.$uptcls.'" role="tabpanel" aria-labelledby="heading'. $accordion_series .'">';
+		$return .= '<div id="collapse'. $group_id. $accordion_series .'" class="panel-collapse collapse '.$uptcls.'" role="tabpanel" aria-labelledby="heading'. $accordion_series .'">';
 		  $return .= '<div class="panel-body">';
 			//$content = apply_filters('the_content', $content);
 			$return .= $content;
