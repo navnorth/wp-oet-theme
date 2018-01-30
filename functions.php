714386dd3bcb33350d805864883747d54f65e570
John Paul Balagolan <johnpaulbalagolan@gmail.com>
2018-01-31 02:07:22 +0800

    remove scroll

--- functions.php
+++ functions.php
@@ -248,7 +248,7 @@ function oet_cf7_footer() {
 	setTimeout(function(){
 	    jQuery('.wpcf7-response-output.wpcf7-validation-errors').attr('tabindex', '0');
 	    jQuery('.wpcf7-response-output.wpcf7-validation-errors').focus();
-	    jQuery(window).scrollTop(jQuery('.wpcf7-response-output.wpcf7-validation-errors').offset().top-120);
+	    //jQuery(window).scrollTop(jQuery('.wpcf7-response-output.wpcf7-validation-errors').offset().top-120);
 	}, 500);
 }, false );
 </script>
