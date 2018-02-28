d951ee9fcb5f4f6c8357192760211bc401a012a3
John Paul Balagolan <johnpaulbalagolan@gmail.com>
2018-03-01 01:21:44 +0800

    update publication display style

--- content-medium.php
+++ content-medium.php
@@ -34,9 +34,11 @@ echo "</div>";
             if (($cnt%3)==1)
                 echo "<div class='row'>";
             ?>
-            <div class="col-md-4 col-sm-6 col-xs-12 medium" style="background:#000000 url(<?php echo $publication->imageUrl; ?>) no-repeat top left;">
-                <h1><a href="<?php echo $publication->url; ?>"><?php echo $publication->name; ?></a></h1>
-                <p><?php echo $publication->description; ?></p>
+            <div class="col-md-4 col-sm-6 col-xs-12">
+                <div class="medium" style="background:#000000 url(<?php echo $publication->imageUrl; ?>) no-repeat top left;">
+                    <h1><a href="<?php echo $publication->url; ?>"><?php echo $publication->name; ?></a></h1>
+                    <p><?php echo $publication->description; ?></p>
+                </div>
             </div>
             <?php
             if (($cnt%3)==0)
