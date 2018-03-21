aa1b8ecba6971e8319ff20dcdbff3dee34cd45ba
John Paul Balagolan <johnpaulbalagolan@gmail.com>
2018-03-21 20:11:30 +0800

    add rss_urls to convert to json

--- content-medium.php
+++ content-medium.php
@@ -20,15 +20,19 @@ $credentials = [
 $medium = new Medium($self_access_token);
 
 $user = $medium->getAuthenticatedUser();
-
+var_dump($user);
 $publications = $medium->publications($user->data->id)->data;
 
+$rss_urls = array(
+            "https://medium.com/feed/@OfficeOfEdTech"
+            );
 ?>
 <div class="col-md-12 col-sm-12 col-xs-12">
     <?php
     if ($publications){
         $cnt = 1;
         foreach($publications as $publication){
+            $rss_urls[] = "https://medium.com/feed/".$publication->name;
             if (($cnt%3)==1)
                 echo "<div class='row'>";
             ?>
@@ -47,4 +51,19 @@ $publications = $medium->publications($user->data->id)->data;
         }
     }
     ?>
+</div>
+<?php
+    var_dump($ss_urls);
+?>
+<div class="col-md-12 col-sm-12 col-xs-12">
+    <div class='row'>
+        <div class="col-md-4 col-sm-6 col-xs-12">
+            <div class="medium" style="background:#000000 url(<?php echo $publication->imageUrl; ?>) no-repeat top left;">
+                <div class="medium-wrapper">
+                    <h1><a href="<?php echo $publication->url; ?>"><?php echo $publication->name; ?></a></h1>
+                    <p><?php echo $publication->description; ?></p>
+                </div>
+            </div>
+        </div>
+   </div>
 </div>
