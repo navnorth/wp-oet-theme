c2fad98b7972cb1cb7e50e84c4304abc5bd06fd8
John Paul Balagolan <johnpaulbalagolan@gmail.com>
2018-05-07 23:34:56 +0800

    fix class display posts to show all

--- content-medium.php
+++ content-medium.php
@@ -97,6 +97,7 @@ foreach ($rss_urls as $rss_url){
 </div>
 <div class="col-md-12 col-sm-12 col-xs-12">
     <?php
+    echo "Test";
     include_once wp_normalize_path( get_stylesheet_directory() . '/classes/oet_medium.php' );
     $oet_medium = new OET_Medium($self_access_token);
     $oet_medium->display_posts();
