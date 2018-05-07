d4a5cb82af3c357169d1a74d7fa11194c47428d0
John Paul Balagolan <johnpaulbalagolan@gmail.com>
2018-05-07 23:21:10 +0800

    add debugging for testing class

--- content-medium.php
+++ content-medium.php
@@ -98,7 +98,7 @@ foreach ($rss_urls as $rss_url){
 <div class="col-md-12 col-sm-12 col-xs-12">
     <?php
     include_once wp_normalize_path( get_stylesheet_directory() . '/classes/oet_medium.php' );
-    
+    var_dump($self_access_token);
     $oet_medium = new OET_Medium($self_access_token);
     var_dump($oet_medium);
     ?>
