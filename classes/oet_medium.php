45981a8fe32c55d9b9336904d4631a7cf2128c38
John Paul Balagolan <johnpaulbalagolan@gmail.com>
2018-10-05 03:04:37 +0800

    debugging only returning 2 publications

--- classes/oet_medium.php
+++ classes/oet_medium.php
@@ -82,7 +82,6 @@ class OET_Medium {
     private function get_feeds($rss_urls = array()){
         if (count($this->_rss_urls)>0) {
             foreach ($this->_rss_urls as $rss_url){
-                var_dump($rss_url['feed_url']);
                 $feed = convert_rss_to_json($rss_url["feed_url"]);
                 if ($feed){
                     if ($feed['status']=="ok"){
@@ -136,6 +135,7 @@ class OET_Medium {
             if (!in_array($value->id,$pubs))
                 unset($this->_publications[$key]);
         }
+        var_dump($this->_publications);
         $pub_only = false;
         if (!empty($pubs))
             $pub_only = true;
