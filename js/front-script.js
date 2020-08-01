a1e0bea7770927462dad4e9097a78bb0610f3fca
Joseph Eneldas <joseph.eneldas@navigationnorth.com>
2020-08-01 12:31:08 +0800

    Update front-script.js

--- js/front-script.js
+++ js/front-script.js
@@ -119,6 +119,11 @@ function openWindow(url, title, width, height) {
 
 
 /* FEATURE VIDEO START */
+var tag = document.createElement('script');
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
 var ytplayer = [];
 function onYouTubeIframeAPIReady() {
 
