3133471d55be13b6003d08ee54d1983b33b2d6c2
John Paul Balagolan <johnpaulbalagolan@gmail.com>
2021-02-19 02:50:31 +0800

    oet shortcodes edit block preview style updates

--- blocks/oet-shortcodes-block/build/index.asset.php
+++ blocks/oet-shortcodes-block/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '8669ce23b6cd5303fdf0af97f8e87a21');
+<?php return array('dependencies' => array('jquery', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'ded4957047698ef494ea98b35f58255e');
