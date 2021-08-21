9e28aec586587e6e825c838b5d2e154bd6bc69c7
josepheneldas <joseph.eneldas@navigationnorth.com>
2021-08-21 09:37:06 +0800

    OET-123 - Styles in Editor don't match Output
    - Fixed issue with accordion, scrolling when clicked.

--- theme-functions/theme-shortcode.php
+++ theme-functions/theme-shortcode.php
@@ -101,7 +101,7 @@ $group_id = "accordion";
 				  $uptcls = '';
 			  }
 
-			  $return .= '<a class="'.$class.'" role="button" data-toggle="collapse" data-parent="#'.$group_id.'" href="#collapse'. $group_id. $accordion_series .'" data-target="#collapse'. $group_id. $accordion_series .'" aria-expanded="false" aria-controls="collapse'. $group_id. $accordion_series .'">';
+			  $return .= '<a class="'.$class.'" role="button" data-toggle="collapse" data-parent="#'.$group_id.'" data-target="#collapse'. $group_id. $accordion_series .'" aria-expanded="false" aria-controls="collapse'. $group_id. $accordion_series .'">';
 			  $return .= $title;
 			$return .= '</a>';
 		 $return .= ' </h5>';
