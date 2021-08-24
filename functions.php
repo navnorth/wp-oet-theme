2806e88db53f6ebaa4e999ef6a880dd814fdc33c
John Paul Balagolan <johnpaulbalagolan@gmail.com>
2021-08-24 21:02:32 +0800

    OET-172 fix slideshow/header display when previewing page

--- functions.php
+++ functions.php
@@ -1031,3 +1031,18 @@ function oet_egg_script() {
 
   return $script;
 }
+
+function oet_fix_post_id_on_preview($null, $post_id) {
+    if (is_preview()) {
+        return get_the_ID();
+    } else {
+        $acf_post_id = isset($post_id->ID) ? $post_id->ID : $post_id;
+
+        if (!empty($acf_post_id)) {
+            return $acf_post_id;
+        } else {
+            return $null;
+        }
+    }
+}
+add_filter( 'acf/pre_load_post_id', 'oet_fix_post_id_on_preview', 10, 2 );
