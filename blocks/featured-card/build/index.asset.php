4b6d762d11912c8341080675f6eda736d6b70c08
John Paul Balagolan <johnpaulbalagolan@gmail.com>
2022-01-27 00:29:13 +0800

    OET-204 Admin block preview

--- blocks/featured-card/build/index.asset.php
+++ blocks/featured-card/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '824a594ed431fcd30e139b17af454341');
+<?php return array('dependencies' => array('jquery', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '67477cb620d3dc38dd13b59ade77ca96');
