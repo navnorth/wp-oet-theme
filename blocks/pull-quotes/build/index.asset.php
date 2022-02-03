3811f03418bb99f859993b29b3d8c8ce36f4f50d
John Paul Balagolan <johnpaulbalagolan@gmail.com>
2022-02-04 00:07:36 +0800

    OET-206 Pull Quotes block settings

--- blocks/pull-quotes/build/index.asset.php
+++ blocks/pull-quotes/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '7716ea06ab862d45ad39e6ba365871c2');
+<?php return array('dependencies' => array('jquery', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '1ca298b5f73337a0bc0e7c212351d87f');
