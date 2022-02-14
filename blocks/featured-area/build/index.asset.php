0421608e499c3669205a4bd5052441649fbd7867
John Paul Balagolan <johnpaulbalagolan@gmail.com>
2022-02-14 17:28:03 +0800

    OET-203 featured area default block preview with import function

--- blocks/featured-area/build/index.asset.php
+++ blocks/featured-area/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '9b0b7c0613b9d937e5b08aa888ffc4d7');
+<?php return array('dependencies' => array('jquery', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '3d9afa18f60ecaec85efeb2bc721b540');
