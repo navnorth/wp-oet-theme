d9ddec90b6bee3c893127b2348392a8e63aef3d1
John Paul Balagolan <johnpaulbalagolan@gmail.com>
2022-02-14 22:03:59 +0800

    OET-208 callout box default block preview

--- blocks/callout-box/build/index.asset.php
+++ blocks/callout-box/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '0f3e89a9b246e6e2b3460485ba2021a7');
+<?php return array('dependencies' => array('jquery', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '1cd1daece705c4c3a2fc22b71467999c');
