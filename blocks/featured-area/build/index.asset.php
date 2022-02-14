15b95a0c5b965b68d3cf1ae6bded5e7f266966b3
John Paul Balagolan <johnpaulbalagolan@gmail.com>
2022-02-14 16:21:21 +0800

    OET-203 featured area admin block preview

--- blocks/featured-area/build/index.asset.php
+++ blocks/featured-area/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '83862a40ccaf154961c0cfce6013071c');
+<?php return array('dependencies' => array('jquery', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '9b0b7c0613b9d937e5b08aa888ffc4d7');
