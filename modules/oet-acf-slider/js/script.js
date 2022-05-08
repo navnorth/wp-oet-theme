80f5ba723dddd021cf340cb9d01e23917a74054b
John Paul Balagolan <johnpaulbalagolan@gmail.com>
2022-05-08 22:24:18 +0800

    OET-230 fix undefined link aria label on acf slider

--- modules/oet-acf-slider/js/script.js
+++ modules/oet-acf-slider/js/script.js
@@ -62,7 +62,7 @@
             jQuery('.bullet:first').addClass('active');
             jQuery('.oet-acf-slider-wrapper ul.slider-list li[data-index="'+indice+'"]').addClass('active');
             jQuery('.bullet:first').attr('aria-label','Slide 1 current slide');
-            var oese_acf_slider_image_alt_text = jQuery('.oet-acf-slider-wrapper ul.slider-list li[data-index="'+indice+'"]').find('.oet_acf_slide_content a img').attr('alt');
+            var oese_acf_slider_image_alt_text = jQuery('.oet-acf-slider-wrapper ul.slider-list li[data-index="'+indice+'"]').find('.oet_acf_slide_content img').attr('alt');
             //var oese_acf_slider_liveregion_title = jQuery('.oet-acf-slider-wrapper ul.slider-list li[data-index="'+indice+'"]').find('h3.oet_acf_slide_title').text();
             jQuery('.oet-acf-slider-wrapper').attr('aria-label','current slide: ' +oese_acf_slider_image_alt_text+ ' link');
         }
