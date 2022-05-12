a3e20b97bac9cb5c9882617310669edd8c02afb6
John Paul Balagolan <johnpaulbalagolan@gmail.com>
2022-05-12 21:59:59 +0800

    OESE-226 disable access to wp-json directory from public but enable it for admin access only

--- functions.php
+++ functions.php
@@ -1151,3 +1151,26 @@ function oet_fix_post_id_on_preview($null, $post_id) {
     }
 }
 add_filter( 'acf/pre_load_post_id', 'oet_fix_post_id_on_preview', 10, 2 );
+
+// Disable access to wp-json from the outside and allow it only for logged in users(WP Admin dashboard)
+function oet_disable_rest_api_from_public($result){
+	// If a previous authentication check was applied, pass that result along without modification.
+    if ( true === $result || is_wp_error( $result ) ) {
+        return $result;
+    }
+
+    // Return an error if user is not logged in.
+    if ( ! is_user_logged_in() ) {
+        return new WP_Error(
+            'rest_not_logged_in',
+            __( 'You are not currently logged in.' ),
+            array( 'status' => 401 )
+        );
+    }
+
+
+    // no effect on logged-in requests
+    return $result;
+}
+add_filter( 'rest_authentication_errors' , 'oet_disable_rest_api_from_public' );
+
