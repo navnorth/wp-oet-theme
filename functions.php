4f3f64494fb7855c4a91bf58b2248e5130ac25dc
John Paul Balagolan <johnpaulbalagolan@gmail.com>
2022-05-24 00:09:49 +0800

    OESE-226 escape request uri

--- functions.php
+++ functions.php
@@ -1160,10 +1160,10 @@ function oet_disable_rest_api_from_public($result){
     }
 
     // Return an error if user is not logged in.
-    if ( ! is_user_logged_in() || false !== stripos( $_SERVER['REQUEST_URI'], 'wp-json/contact-form-7' ) ) {
+    if ( ! is_user_logged_in() || false !== stripos( esc_url_raw($_SERVER['REQUEST_URI']), '/wp-json/contact-form-7' ) ) {
         return new WP_Error(
             'rest_not_logged_in',
-            __( 'You are not currently logged in.' . $_SERVER['REQUEST_URI'] ),
+            __( 'You are not currently logged in.' . esc_url_raw($_SERVER['REQUEST_URI']) ),
             array( 'status' => 401 )
         );
     }
