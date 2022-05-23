859829827bfd256d56d150758960003729ccd471
John Paul Balagolan <johnpaulbalagolan@gmail.com>
2022-05-24 00:19:48 +0800

    OESE-226 request uri position debugging

--- functions.php
+++ functions.php
@@ -1163,7 +1163,7 @@ function oet_disable_rest_api_from_public($result){
     if ( ! is_user_logged_in() || false !== strpos( esc_url_raw($_SERVER['REQUEST_URI']), '\/wp-json\/contact-form-7' ) ) {
         return new WP_Error(
             'rest_not_logged_in',
-            __( 'You are not currently logged in.' . esc_url_raw($_SERVER['REQUEST_URI']) . ' ' .strpos( esc_url_raw($_SERVER['REQUEST_URI']), '\/wp-json\/contact-form-7' ) ),
+            __( 'You are not currently logged in.' . esc_url_raw($_SERVER['REQUEST_URI']) . ' ' .print_r(strpos( esc_url_raw($_SERVER['REQUEST_URI']), '\/wp-json\/contact-form-7' ),true) ),
             array( 'status' => 401 )
         );
     }
