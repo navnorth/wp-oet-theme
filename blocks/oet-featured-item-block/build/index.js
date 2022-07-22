2671eb56683dfa96cc71e858aff88cbb35707179
John Paul Balagolan <johnpaulbalagolan@gmail.com>
2022-07-22 12:34:16 +0800

    OET-235 another debugging on empty headings of the Featured Item block

--- blocks/oet-featured-item-block/build/index.js
+++ blocks/oet-featured-item-block/build/index.js
@@ -405,7 +405,6 @@
                         t.oetblkTitleTag="h3";
                     if (t.oetblkDateTag=="" || typeof t.oetblkDateTag === 'undefined')
                         t.oetblkDateTag="h4";
-                    console.log(t);
                     return (0, a.createElement)(
                         "div",
                         null,
@@ -424,8 +423,8 @@
                                         { class: "oet-featured-item-image-wrapper-float-left", style: void 0 !== t.thumbnailsize ? { width: t.thumbnailsize + "%" } : { margin: "0" } },
                                         t.mediaURL && (0, a.createElement)("img", { src: t.mediaURL, class: "featured_item_image oet-featured-item-image", alt: "" })
                                     ),
-                                    (void 0 === t.oetblkFeaturedItemTitle && "" != t.oetblkFeaturedItemTitle ) ? (0, a.createElement)(t.oetblkTitleTag, { class: "oet-featured-item-title oet-featured-item-title-ytr85g9wer has-oet-color-pallete-orange-color has-text-color" }, t.oetblkFeaturedItemTitle): void 0,
-                                    (void 0 === t.oetblkFeaturedItemDate && "" != t.oetblkFeaturedItemDate ) ? (0, a.createElement)(t.oetblkDateTag, { class: "oet-featured-item-date oet-featured-item-date-ytr85g9wer has-oet-color-pallete-black-color has-text-color" }, t.oetblkFeaturedItemDate): void 0,
+                                    (void 0 === t.oetblkFeaturedItemTitle && "" !== t.oetblkFeaturedItemTitle ) ? (0, a.createElement)(t.oetblkTitleTag, { class: "oet-featured-item-title oet-featured-item-title-ytr85g9wer has-oet-color-pallete-orange-color has-text-color" }, t.oetblkFeaturedItemTitle): void 0,
+                                    (void 0 === t.oetblkFeaturedItemDate && "" !== t.oetblkFeaturedItemDate ) ? (0, a.createElement)(t.oetblkDateTag, { class: "oet-featured-item-date oet-featured-item-date-ytr85g9wer has-oet-color-pallete-black-color has-text-color" }, t.oetblkFeaturedItemDate): void 0,
                                     (0, a.createElement)(o.InnerBlocks.Content, null)
                                 )
                             )
