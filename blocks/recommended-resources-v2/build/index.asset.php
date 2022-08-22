55b1bcfb0620209863a54135d25d098eec7b4486
John Paul Balagolan <johnpaulbalagolan@gmail.com>
2022-08-22 18:09:33 +0800

    OET-233 add the heading option to the setting

--- blocks/recommended-resources-v2/build/index.asset.php
+++ blocks/recommended-resources-v2/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery', 'wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '452e3cb1681b7de11f4f');
+<?php return array('dependencies' => array('jquery', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '934ec7a625008ebd616f');
