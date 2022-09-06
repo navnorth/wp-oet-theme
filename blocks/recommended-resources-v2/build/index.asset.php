228d925f0711ec6104b3a3375682b2b07274c5a3
John Paul Balagolan <johnpaulbalagolan@gmail.com>
2022-09-06 10:34:03 +0800

    OET-233 update block.json and display of block on edit

--- blocks/recommended-resources-v2/build/index.asset.php
+++ blocks/recommended-resources-v2/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'af22479447a4f276f33e');
+<?php return array('dependencies' => array('jquery', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'c9b7192103ea1083122f');
