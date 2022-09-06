39318bd9e37ebf15489ab1835658a0575f17dc21
John Paul Balagolan <johnpaulbalagolan@gmail.com>
2022-09-06 13:41:15 +0800

    OET-233 display default Recommended resources block display with import functionality

--- blocks/recommended-resources-v2/build/index.asset.php
+++ blocks/recommended-resources-v2/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'c9b7192103ea1083122f');
+<?php return array('dependencies' => array('jquery', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '7b53c08cc2f4238d8b48');
