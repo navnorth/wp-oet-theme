01197668823f6153b7302ee3d5e21d7d14d95d92
John Paul Balagolan <johnpaulbalagolan@gmail.com>
2022-09-06 09:58:41 +0800

    OET-233 add media link, type, text, and source setting on Recommended Resources block

--- blocks/recommended-resources-v2/build/index.asset.php
+++ blocks/recommended-resources-v2/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '934ec7a625008ebd616f');
+<?php return array('dependencies' => array('jquery', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'af22479447a4f276f33e');
