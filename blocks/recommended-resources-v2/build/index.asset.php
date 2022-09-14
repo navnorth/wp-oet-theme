b4447c19ab100c7f7a7547a81a66eab8a8b51ceb
John Paul Balagolan <johnpaulbalagolan@gmail.com>
2022-09-14 20:21:27 +0800

    OET-233 import functionality of recommended resources block

--- blocks/recommended-resources-v2/build/index.asset.php
+++ blocks/recommended-resources-v2/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '7b53c08cc2f4238d8b48');
+<?php return array('dependencies' => array('jquery', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '6d5dc60804d1a74baa6d');
