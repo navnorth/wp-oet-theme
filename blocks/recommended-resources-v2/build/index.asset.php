e5ddfb91fa16762a93b836b9051f6a4d6c8405fb
John Paul Balagolan <johnpaulbalagolan@gmail.com>
2022-10-04 00:05:06 +0800

    OET-233 import function and edit mode block display

--- blocks/recommended-resources-v2/build/index.asset.php
+++ blocks/recommended-resources-v2/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '6d5dc60804d1a74baa6d');
+<?php return array('dependencies' => array('jquery', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '435e56c6e1922ac98ce9');
