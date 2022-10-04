d1e6eec93adb34ef11b87ef1096a334e894c20eb
John Paul Balagolan <johnpaulbalagolan@gmail.com>
2022-10-04 22:42:09 +0800

    OET-233 update save button style

--- blocks/recommended-resources/build/index.asset.php
+++ blocks/recommended-resources/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '539e6ba877c64f93adf7');
+<?php return array('dependencies' => array('jquery', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '79d786011927dbca23c1');
