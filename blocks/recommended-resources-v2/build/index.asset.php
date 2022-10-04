6b6de5f5881df1bd8e05e7646595d4eff25634e8
John Paul Balagolan <johnpaulbalagolan@gmail.com>
2022-10-04 10:27:40 +0800

    OET-233 block preview display on edit mode

--- blocks/recommended-resources-v2/build/index.asset.php
+++ blocks/recommended-resources-v2/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '435e56c6e1922ac98ce9');
+<?php return array('dependencies' => array('jquery', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '59e6ddd8a5c069b89c81');
