e463eddd4308866121c6242225859224062b3c21
John Paul Balagolan <johnpaulbalagolan@gmail.com>
2022-10-04 15:44:33 +0800

    OET-233 additional style updates

--- blocks/recommended-resources-v2/build/index.asset.php
+++ blocks/recommended-resources-v2/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '13e363b3ed18d985a919');
+<?php return array('dependencies' => array('jquery', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '580f78ca356ec39cd53a');
