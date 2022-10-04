fd0f2519e45d686ec80c02118f1424d5ae9295df
John Paul Balagolan <johnpaulbalagolan@gmail.com>
2022-10-04 22:14:02 +0800

    OET-233 debug loading of recommended resources block for the test site

--- blocks/recommended-resources/init.php
+++ blocks/recommended-resources/init.php
@@ -129,13 +129,13 @@ if (!function_exists('is_version_58')) {
 }
 
 // Checks WP version to register block via block json if version is 5.8 or later
-if ( is_version_58() ) {
+//if ( is_version_58() ) {
     add_action( 'init', 'oet_recommended_resources_block_init' );
     //add_action( 'init', 'oet_recommended_resources_block_json_init' );
-} else {
+//} else {
     //add_action( 'init', 'oet_recommended_resources_block_init' );
-    add_action( 'init', 'oet_recommended_resources_block_json_init' );
-}
+//    add_action( 'init', 'oet_recommended_resources_block_json_init' );
+//}
 
 // Display Recommended Resources Block
 /**
