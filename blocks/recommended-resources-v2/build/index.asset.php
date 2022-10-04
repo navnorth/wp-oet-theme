399fa85ab1d75322f0edceda1bf0a03d7e58b728
John Paul Balagolan <johnpaulbalagolan@gmail.com>
2022-10-04 15:11:08 +0800

    OET-233 frontend block display

--- blocks/recommended-resources-v2/build/index.asset.php
+++ blocks/recommended-resources-v2/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '59e6ddd8a5c069b89c81');
+<?php return array('dependencies' => array('jquery', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '13e363b3ed18d985a919');
