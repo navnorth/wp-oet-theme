acd93c3f023e61cd38f032962a61d7d13d6c34b7
John Paul Balagolan <johnpaulbalagolan@gmail.com>
2023-03-17 15:09:00 +0800

    OET-266 fix registering of footnotes block on WP 5.7.5

--- blocks/footnotes/build/index.asset.php
+++ blocks/footnotes/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'ecbde7939f6427576735');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'c7a02f7cbdede1ff0e3e');
